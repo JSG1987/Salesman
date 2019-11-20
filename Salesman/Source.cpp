4137cb0ece70b2e9e1a18328e6f631c52155edf3
JSG1987 <JGentry0058@gmail.com>
2019-11-19 18:58:20 -0600

    salesmen2.0

--- Salesman/Source.cpp
+++ Salesman/Source.cpp
@@ -11,9 +11,12 @@ using namespace std;
 ifstream infile;
 ofstream outfile;
 
-
-void print(float ary[11]);
+float avg(float x, float y);
+void print(float ary[11],float bry[12]);
 void ary(float ary[11]);
+float commission(float salestotal);
+void firedEmployee(float x, float y);
+
 int main()
 {
 	infile.open("Salesperson.txt");
@@ -21,23 +24,49 @@ int main()
 	if (!infile.good())
 	{
 		cout << "file bad" << endl;
+
 	}
+
 	int a;
 	float b;
+	float cnt[12];
 	float totalSales = 0.0;
 	float salesTotal[11];
+
 	ary(salesTotal);
+	ary(cnt);
+
 	while (infile)
 	{
 		infile >> a >> b;
 		
 		totalSales += b;
 		salesTotal[a] += b;
+		cnt[a]++;
+		
+		
 	}
-	print(salesTotal);
-	cout << totalSales << endl;
+	//int temp = salesTotal[1];
+	int c;
+	for (int i = 1; i < 11; i++) 
+	{
+		if (salesTotal[i] < salesTotal[i + i])
+		{
+			c = i;
+		}
+		
+	}
+	
+	
+	print(salesTotal,cnt);
+	outfile << "Company Sales Total " << totalSales << endl;
+	outfile << "employee "<< c << " is getting fired " << endl;
+	
+	
+	
+	
 
-
+	//avg(salesTotal[11], cnt[12]);
 
 	outfile.close();
 	infile.close();
@@ -45,13 +74,14 @@ int main()
 	return 0;
 }
 
-void print(float ary[11])
+void print(float ary[11],float bry[12])
 {
-
+	cout << "Employee #  ||" << " Total sales ||" << " avregaes ||"<< " Commission||"<< endl;
 	for (int i = 1; i < 11; i++)
 	{
-		cout << i << " ";
-		cout << ary[i] << endl;
+		cout << fixed<<setprecision(2);
+		cout << setw(5) << left << i << setw(5) << "       || " << left<< ary[i] << "       ||  " << setw(5) <<left<< avg(ary[i], bry[i]) << setw(5)<<left<< " ||    " << commission(ary[i]) << endl;
+
 	}
 
 }
@@ -62,5 +92,22 @@ void ary(float ary[11])
 	{
 		ary[i] = 0;
 	}
+
+}
+float avg(float x, float y)
+{
+
+	return x / y;
+}
+float commission(float salestotal)
+{
+	float commission = (salestotal*.09) + (800);
+
+	return commission;
+
+}
+void firedEmployee(float x, float y)
+{
+
 
 }
